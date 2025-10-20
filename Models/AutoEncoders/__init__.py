90a35c3f7c2720da4f727e5f9c54a1040d0b8eeb
Yasantha Niroshan <niroshanyi@gmail.com>
2025-10-20 19:37:20 +0530

    Fixed Minor Bug - Removed dropout_rate from the decoder

--- Models/AutoEncoders/__init__.py
+++ Models/AutoEncoders/__init__.py
@@ -6,7 +6,7 @@ class SimpleAutoencoder(nn.Module):
     def __init__(self, latent_size=32, dropout_rate=0.1):
         super(SimpleAutoencoder, self).__init__()
         self.encoder = SimpleEncoder(latent_size, dropout_rate)
-        self.decoder = SimpleDecoder(latent_size, dropout_rate)
+        self.decoder = SimpleDecoder(latent_size)
 
     def forward(self, x):
         x = self.encoder(x)
