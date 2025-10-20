af95fa79c6a4d23b8f75296327c7667f02976bcc
Yasantha Niroshan <niroshanyi@gmail.com>
2025-10-20 19:31:48 +0530

    Added Minor Change - Added droupout rate to initialization

--- Models/AutoEncoders/__init__.py
+++ Models/AutoEncoders/__init__.py
@@ -3,10 +3,10 @@ from Models.Encoders import SimpleEncoder
 from Models.Decoders import SimpleDecoder
 
 class SimpleAutoencoder(nn.Module):
-    def __init__(self, latent_size=32):
+    def __init__(self, latent_size=32, dropout_rate=0.1):
         super(SimpleAutoencoder, self).__init__()
-        self.encoder = SimpleEncoder(latent_size)
-        self.decoder = SimpleDecoder(latent_size)
+        self.encoder = SimpleEncoder(latent_size, dropout_rate)
+        self.decoder = SimpleDecoder(latent_size, dropout_rate)
 
     def forward(self, x):
         x = self.encoder(x)
